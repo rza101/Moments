c7b0d1d5c12e9b44b8c61e6f2297ce3f7d0830a5
Fandy <100325135+Fandy1566@users.noreply.github.com>
2022-12-25 10:33:43 +0700

    add super toast and some validation

--- Android/Moments/app/src/main/java/com/pahat/moments/ui/activities/createpost/CreatePostActivity.java
+++ Android/Moments/app/src/main/java/com/pahat/moments/ui/activities/createpost/CreatePostActivity.java
@@ -1,13 +1,20 @@
 package com.pahat.moments.ui.activities.createpost;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
 import com.pahat.moments.databinding.ActivityCreatePostBinding;
+import com.pahat.moments.ui.activities.forgotpass.ForgotPassActivity;
 import com.pahat.moments.util.Utilities;
 
 import java.io.File;
@@ -25,6 +32,29 @@ public class CreatePostActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Utilities.initChildToolbar(this, binding.toolbar, "Create Post");
+
+        binding.createPostBtnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String caption = binding.createPostEtCaption.getText().toString();
+                Drawable image = binding.createPostIvPreview.getDrawable();
+                if (TextUtils.isEmpty(caption)) {
+                    binding.createPostEtCaption.setError("Enter your email address!");
+                    return;
+                }
+                if (image == null) {
+                    SuperActivityToast.create(CreatePostActivity.this, new Style(), Style.TYPE_STANDARD)
+                            .setText("Please enter an image")
+                            .setDuration(Style.DURATION_LONG)
+                            .setFrame(Style.FRAME_STANDARD)
+                            .setColor(Color.parseColor("#3FBDF1"))
+                            .setAnimations(Style.ANIMATIONS_FADE)
+                            .show();
+//                    Toast.makeText(CreatePostActivity.this, "Please enter an image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 
     private File createTempImageFile(Context context) {
--- Android/Moments/app/src/main/java/com/pahat/moments/ui/activities/updatepost/UpdatePostActivity.java
+++ Android/Moments/app/src/main/java/com/pahat/moments/ui/activities/updatepost/UpdatePostActivity.java
@@ -1,11 +1,15 @@
 package com.pahat.moments.ui.activities.updatepost;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.pahat.moments.databinding.ActivityUpdatePostBinding;
+import com.pahat.moments.ui.activities.createpost.CreatePostActivity;
 import com.pahat.moments.util.Utilities;
 
 public class UpdatePostActivity extends AppCompatActivity {
@@ -20,5 +24,16 @@ public class UpdatePostActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Utilities.initChildToolbar(this, binding.toolbar, "Update Post");
+
+        binding.updatePostBtnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String caption = binding.updatePostEtCaption.getText().toString();
+                if (TextUtils.isEmpty(caption)) {
+                    binding.updatePostEtCaption.setError("Enter your email address!");
+                    return;
+                }
+            }
+        });
     }
 }
